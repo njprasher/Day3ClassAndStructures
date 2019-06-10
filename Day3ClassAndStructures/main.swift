88532cdc7ce5b8dd09d490b1ae3389b331f1cfab
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-06-10 18:33:09 -0400

    Initial Commit

---
+++ Day3ClassAndStructures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day3ClassAndStructures
+//
+//  Created by Neeraj Prasher on 2019-06-10.
+//  Copyright © 2019 lambton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
