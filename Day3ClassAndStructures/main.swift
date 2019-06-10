d945666397645639829437c6b46be46fc201e115
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-06-10 18:54:49 -0400

    [Neeraj] Classes in Swift with nil value example

--- Day3ClassAndStructures/main.swift
+++ Day3ClassAndStructures/main.swift
@@ -10,3 +10,29 @@ import Foundation
 
 print("Hello, World!")
 
+class Student
+{
+    var studentId: Int = 0
+    var firstName: String?
+    var lastName: String!
+    var gender: Bool = false
+    var result: String = ""
+    
+    func display() {
+        print("Student ID : \(self.studentId)")
+        print("First Name : \(self.firstName)")
+        print("Last Name : \(self.lastName)")
+        print("Gender : \(self.gender)")
+        print("Result : \(self.result)")
+    }
+}
+
+var s1 = Student()
+
+var s2: Student
+
+s2 = Student()
+
+s1.display()
+
+s2.display()
